1774f271f9c716ef11da5f5fe71826887a0441f3
dlwhdtj2284 <dlwhdtj2284@naver.com>
2022-12-29 00:01:14 +0900

    Add media query

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,27 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Search from "./Search";
+import { useMediaQuery } from "react-responsive"
+
 
 function App() {
+  const isPc = useMediaQuery({
+    query: "(min-width:720px)"
+  })
+
+  const WebSize = isPc ? "급퉁pc" : "급퉁phone"
+
+
   return (
     <div className="App">
       <Search />
+      <div>
+        {WebSize}
+      </div>
     </div>
   );
+
+
 }
 
 export default App;
