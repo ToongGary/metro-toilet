9d1f7bbaa3352cc9e682bd7383fda55ed06790f0
LeeShinYeoung <iqrash12@gmail.com>
2023-01-21 16:50:13 +0900

    Refactor data import

---
+++ src/commons/contexts/stations.context.tsx
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { createContext, useEffect, useState } from 'react';
+import { Station } from '../interfaces/station.interface';
+
+export const StationsContext = createContext<StationsContextValue>({});
+
+export interface StationsContextValue {
+  [id: string]: Station;
+}
+
+export function StationsContextProvider({
+  children
+}: {
+  children: JSX.Element | JSX.Element[];
+}) {
+  const [stations, setStations] = useState<StationsContextValue>({});
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/metro/station')
+      .then((response) => {
+        const stations: StationsContextValue = {};
+
+        for (const station of response.data) {
+          stations[station.id] = station as Station;
+        }
+
+        setStations(stations);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <StationsContext.Provider value={stations}>
+      {children}
+    </StationsContext.Provider>
+  );
+}
