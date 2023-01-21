e0f289aaed898f0e8c8bca0542a7b417440db4d1
LeeShinYeoung <iqrash12@gmail.com>
2023-01-21 16:48:08 +0900

    Refactor interfaces

---
+++ src/commons/interfaces/station-toilet.interface.ts
@@ -0,0 +1,3 @@
+export interface StationToilet {
+  id: number;
+}
--- src/commons/interfaces/station.interface.ts
+++ src/commons/interfaces/station.interface.ts
@@ -1,4 +1,4 @@
-export default interface Metro {
+export interface Station {
   id: number;
   lineCode: string;
   lineName: string;
