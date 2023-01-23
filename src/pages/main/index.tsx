0668e804d3ffc9096553710e2809be52973ed5ac
LeeShinYeoung <iqrash12@gmail.com>
2023-01-24 00:55:27 +0900

    Apply subway map to the main page

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,10 +1,13 @@
 import { Header } from '../../commons/components/header';
+import { ReactComponent as SubwaySeoul } from '../../commons/resources/subway-seoul.svg';
+import styles from './styles.module.css';
 
 export function Main() {
   return (
-    <div>
+    <div className={styles.main}>
       <Header></Header>
-      <div>Main Page</div>
+      <SubwaySeoul width="2800px" height="100%" />
+      <div></div>
     </div>
   );
 }
